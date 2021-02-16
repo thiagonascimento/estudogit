9173e0dc7ca4db73344c014a9a6168cf181678e5
thiago.nascimento <carmo_thiago@yahoo.com.br>
2021-02-15 21:58:13 -0300

    ptimeiro comitt

---
+++ projetoteste/src/main/java/br/com/teste/projetoteste/ProjetotesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotesteApplication.class, args);
+	}
+
+}
---
+++ projetoteste/src/test/java/br/com/teste/projetoteste/ProjetotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
